d140f40bd4f0a49350564410ab9a65783de05af8
Helenajuhler <110617908+Helenajuhler@users.noreply.github.com>
2023-03-13 19:57:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function redirecionarParaPagina ()  
+{
+window.location.href="Personal.html";
+}
+
+function redirecionarParaPagina ()  
+{
+window.location.href="Academic.html";
+}
+
+function redirecionarParaPagina ()  
+{
+window.location.href="Professional.html";
+}
+
+function redirecionarParaPagina ()  
+{
+window.location.href="Combined.html";
+}
+
+function redirecionarParaPagina ()  
+{
+window.location.href="Settings.html";
+}
+
+function redirecionarParaPagina ()  
+{
+window.location.href="Login.html";
+}
